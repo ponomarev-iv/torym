66232103e35b72340db98ec604379cf4fa95c538
Pompo86 <ilya86ponomarev@gmail.com>
2016-11-21 23:07:45 +0500

    Сделал адаптацию главной страницы, переключение карты

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -18,13 +18,77 @@ function swiperInit(){
             slidesPerView: 4,
             nextButton: '.swiper-button-next',
             prevButton: '.swiper-button-prev',
-            spaceBetween: 30
+            spaceBetween: 30,
+            breakpoints: {
+                991: {
+                    slidesPerView: 3,
+                    spaceBetween: 20
+                },
+                768:{
+                    slidesPerView: 2,
+                    spaceBetween: 10
+                },
+                480:{
+                    slidesPerView: 1
+                }
+            }
         });
     } else{
         return false;
     }
 }
 
+function swiperPartner(){
+    var windowWidth = document.documentElement.clientWidth;
+
+    if($('#partner-swiper').length){
+
+
+        if(windowWidth <= 480){
+                var partnerSwiper = new Swiper('#partner-swiper', {
+                autoplay: 2500,
+                nextButton: '.swiper-button-next',
+                prevButton: '.swiper-button-prev',
+                loop: true
+            });
+        }
+        else {
+            return false;
+        }
+
+    } else{
+        return false;
+    }
+}
+
+function switchMap(){
+    var switchLink = $('.js-map-switch'),
+        switchTab = $('.js-tab');
+
+    $(switchLink).on('click', function () {
+        if($(this).hasClass('is-active')){
+            return false;
+        }else{
+            $(switchTab).removeClass('is-active');
+            $(switchLink).removeClass('is-active');
+            $(this).addClass('is-active');
+            var tabLink = $(this).data('href');
+            $(tabLink).addClass('is-active');
+            $('.js-map').toggleClass('map_sklad');
+        }
+    })
+}
+
+$(window).resize(function(){
+    swiperPartner();
+});
+
+$(window).resize();
+
 $(document).ready(function(){
     swiperInit();
+    swiperPartner();
+    switchMap();
 });
+
+
