51bec39959e7db1cc9992034276535fb8d3704fa
ponomarev86 <ilya86ponomarev@gmail.com>
2017-11-29 09:26:14 +0500

    Правки от дизайнера

--- public/send.php
+++ public/send.php
@@ -7,13 +7,10 @@ $subject   = "Новая заявка с сайта \"$sitename\"";
 
 $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
-$email = trim($_POST["email"]);
-
 
 $message = "
 Имя: $name <br>
-Телефон: $phone <br>
-E-mail: $email <br>
+Телефон: $phone
 ";
 
 mail($recepient, $subject, $message, "X-Mailer: PHP/" . phpversion() . "\r\n" . "Content-type: text/html; charset=\"utf-8\"");
